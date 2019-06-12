52e21d01beb6f21bb41355428e421b537ede8c25
OnAaron <garcia.muniz.aaron@gmail.com>
2019-06-12 17:29:52 +0200

    comunicacion

--- src/intel/pract/Agente_Cliente.java
+++ src/intel/pract/Agente_Cliente.java
@@ -2,6 +2,9 @@ package intel.pract;
 
 import jade.core.behaviours.CyclicBehaviour;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 import jade.core.Agent;
@@ -11,16 +14,41 @@ public class Agente_Cliente extends Agent{
 	private static final long serialVersionUID = 1L;
 
 	public void setup() {
+		final File archivo;
+		final FileReader fr;
+		final BufferedReader br;
+		String content = null;
 		System.out.println("Soy el agente Cliente");
 		addBehaviour(new CyclicBehaviour(this){
 			private static final long serialVersionUID = 1L;
 
 			public void action() {
+				try {
 				Scanner scanner=new Scanner(System.in);
 				System.out.print("Introduzca el fichero a buscar: ");
 				String fich = scanner.nextLine();
-				System.out.println(temp);
-				Comunicaciones.enviarMensaje(this.myAgent, "busqueda", temp);
+				archivo = new File(fich);
+				String ruta = archivo.getAbsolutePath();
+				archivo = new File(ruta+"/"+fich);
+				fr = new FileReader(archivo);
+				br = new BufferedReader(fr);
+				String linea;
+				while((linea=br.readLine())!=null)
+					content += linea;
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+				finally{
+			         try{                    
+			            if( null != fr ){   
+			               fr.close();     
+			            }                  
+			         }catch (Exception e2){ 
+			            e2.printStackTrace();
+			         }
+			         Comunicaciones.enviarMensaje(this.myAgent, "busqueda", content);
+				}
 			}
 		});
 	}
