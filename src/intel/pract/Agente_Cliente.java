3a5b06c2f09735e11eb549ec616f51ae656b319e
OnAaron <garcia.muniz.aaron@gmail.com>
2019-06-12 17:43:46 +0200

    fichero entero

--- src/intel/pract/Agente_Cliente.java
+++ src/intel/pract/Agente_Cliente.java
@@ -1,10 +1,7 @@
 package intel.pract;
 
 import jade.core.behaviours.CyclicBehaviour;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
 
 import jade.core.Agent;
@@ -14,42 +11,38 @@ public class Agente_Cliente extends Agent{
 	private static final long serialVersionUID = 1L;
 
 	public void setup() {
-		final File archivo;
-		final FileReader fr;
-		final BufferedReader br;
-		String content = null;
 		System.out.println("Soy el agente Cliente");
 		addBehaviour(new CyclicBehaviour(this){
 			private static final long serialVersionUID = 1L;
 
 			public void action() {
-				try {
 				Scanner scanner=new Scanner(System.in);
 				System.out.print("Introduzca el fichero a buscar: ");
-				String fich = scanner.nextLine();
-				archivo = new File(fich);
-				String ruta = archivo.getAbsolutePath();
-				archivo = new File(ruta+"/"+fich);
-				fr = new FileReader(archivo);
-				br = new BufferedReader(fr);
-				String linea;
-				while((linea=br.readLine())!=null)
-					content += linea;
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-				}
-				finally{
-			         try{                    
-			            if( null != fr ){   
-			               fr.close();     
-			            }                  
-			         }catch (Exception e2){ 
-			            e2.printStackTrace();
-			         }
-			         Comunicaciones.enviarMensaje(this.myAgent, "busqueda", content);
-				}
+				String temp=scanner.nextLine();
+				String content = getContenido(temp);
+				System.out.println(content);
+				Comunicaciones.enviarMensaje(this.myAgent, "busqueda", content);
 			}
 		});
+	}
+	public String getContenido(String file) {
+		try {
+			File archivo;
+			FileReader fr;
+			BufferedReader br;
+			String content = "";
+			archivo = new File(file);
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+			String linea;
+			while((linea=br.readLine())!=null)
+				content += linea;
+			br.close();
+			return content;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
